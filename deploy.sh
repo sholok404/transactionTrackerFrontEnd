ad0a49143a393e23ab0d1c03e0f186e3636fc06a
Syed Subeh-Sadik Sholok <sholok@localhost.localdomain>
2023-07-30 01:13:32 +0600

    Merged with buildingCards

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 read MESSAGE
 
+rm -rf dist
 npx parcel build src/index.html --public-url https://sholok404.github.io/transactionTrackerFrontEnd/
 git add dist && git commit -m "$MESSAGE"
 git subtree push --prefix dist origin dist
