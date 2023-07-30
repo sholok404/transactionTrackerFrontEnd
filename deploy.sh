c13ca67c6a512414f97ed77a449acc95b975276d
Syed Subeh-Sadik Sholok <sholok@localhost.localdomain>
2023-07-31 00:18:02 +0600

    removed duplicate

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read MESSAGE
-
-rm -rf dist
-npx parcel build src/index.html --public-url https://sholok404.github.io/transactionTrackerFrontEnd/
-git add dist && git commit -m "$MESSAGE"
-git subtree push --prefix dist origin dist
